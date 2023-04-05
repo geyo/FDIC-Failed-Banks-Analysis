f496364f2c758d45859f013f5ff9a42f43f06be2
NicoleBusta <tropical106@surewest.net>
2023-04-05 16:20:59 -0700

    adding updated js for alphabetized banks

--- static/js/bankdropdowndemo.js
+++ static/js/bankdropdowndemo.js
@@ -1,8 +1,8 @@
 //--------- Bank DropDown and Demographics ---------//
 
 // Load the CSV file using d3.csv
-d3.json('/banklist_for_dropdowndemo').then(function (data) {
-//d3.csv('source_data/banklist_for_dropdowndemo.csv').then(function (data) {
+// d3.json('banklist_for_dropdowndemo').then(function (data) {
+d3.csv('source_data/banklist_for_dropdowndemo.csv').then(function (data) {
     // Parse the CSV data into an array of objects
     var parsedData = data.map(function (row) {
         return {
@@ -30,74 +30,43 @@ d3.json('/banklist_for_dropdowndemo').then(function (data) {
 
     // Attach an event listener to the dropdown menu
     bankDropdown.on('change', function () {
-        d3.select('#sample-metadata').html('')
-        // Get the selected option
-        var selectedOption = bankDropdown.property('value');
-
-        // Filter the data to only include rows that match the selected option
-        var filteredData = parsedData.filter(function (row) {
-            return row['bank_name'] === selectedOption;
-        })[0];
-
-        // Use D3 to create an HTML table element and populate it with the filtered data
-        var table = d3.select('#sample-metadata')
-            .append('table');
-
-        var headers = ['bank_name', 'city', 'state', 'cert', 'acquiring_institution', 'closing_date', 'fund', 'total_assets', 'total_liabilities', 'net_worth_deficit'];
-        var headers_nice = {'bank_name': 'Bank Name',
-                            'city': 'City', 
-                            'state': 'State', 
-                            'cert': 'Cert', 
-                            'acquiring_institution': 'Acquiring Institution', 
-                            'closing_date': 'Closing Date', 
-                            'fund': 'Fund', 
-                            'total_assets': 'Total Assets', 
-                            'total_liabilities': 'Total Liabilities', 
-                            'net_worth_deficit': 'Net Worth Deficit'};
-
-        // console.log(filteredData);
-        headers.forEach(function (header) {
-            let headerRow = table.append('tr');
-            //headerRow.append('th')
-                //.text(header);
-            headerRow.append('td').text(headers_nice[header])
-            headerRow.append('td').text(filteredData[header])
-        });
-
-        // var rows = table.selectAll('tr')
-        //     .data(filteredData)
-        //     .enter()
-        //     .append('tr')
-        //     // .attr('class', 'data-row');
-
-        // rows.append('td')
-        //     .text(function (d) { return d.bank_name; });
-
-        // rows.append('td')
-        //     .text(function (d) { return d.city; });
-
-        // rows.append('td')
-        //     .text(function (d) { return d.state; });
-
-        // rows.append('td')
-        //     .text(function (d) { return d.cert; });
-
-        // rows.append('td')
-        //     .text(function (d) { return d.acquiring_institution; });
-
-        // rows.append('td')
-        //     .text(function (d) { return d.closing_date; });
-
-        // rows.append('td')
-        //     .text(function (d) { return d.fund; });
-
-        // rows.append('td')
-        //     .text(function (d) { return d.total_assets; });
-
-        // rows.append('td')
-        //     .text(function (d) { return d.total_liabilities; });
-
-        // rows.append('td')
-        //     .text(function (d) { return d.net_worth_deficit; });
+        loadBankdata()
     });
+
+    loadBankdata()
+    function loadBankdata() {
+        d3.select('#sample-metadata').html('')
+            // Get the selected option
+            var selectedOption = bankDropdown.property('value');
+    
+            // Filter the data to only include rows that match the selected option
+            var filteredData = parsedData.filter(function (row) {
+                return row['bank_name'] === selectedOption;
+            })[0];
+    
+            // Use D3 to create an HTML table element and populate it with the filtered data
+            var table = d3.select('#sample-metadata')
+                .append('table');
+    
+            var headers = ['bank_name', 'city', 'state', 'cert', 'acquiring_institution', 'closing_date', 'fund', 'total_assets', 'total_liabilities', 'net_worth_deficit'];
+            var headers_nice = {'bank_name': 'Bank Name',
+                                'city': 'City', 
+                                'state': 'State', 
+                                'cert': 'Cert', 
+                                'acquiring_institution': 'Acquiring Institution', 
+                                'closing_date': 'Closing Date', 
+                                'fund': 'Fund', 
+                                'total_assets': 'Total Assets', 
+                                'total_liabilities': 'Total Liabilities', 
+                                'net_worth_deficit': 'Net Worth Deficit'};
+    
+            // console.log(filteredData);
+            headers.forEach(function (header) {
+                let headerRow = table.append('tr');
+                //headerRow.append('th')
+                    //.text(header);
+                headerRow.append('td').text(headers_nice[header])
+                headerRow.append('td').text(filteredData[header])
+            });
+    }
 });
